d310774dec1a641b917df5c6dd981fce4b2e18ad
Gleb Bondarenko <renkornator@gmail.com>
2023-08-09 11:14:18 +0300

    initial

---
+++ server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var pairs = []string{
+	"NEAR/USDT", "XLM/USDT", "XRP/USDT", "DOGE/USDT", "ADA/USDT", "SOL/USDT",
+	"XMR/USDT", "MRK/USDT", "SAND/USDT", "XTZ/USDT", "CRV/USDT", "DASH/USDT",
+	"SC/USDT", "AUDIO/USDT", "DODO/USDT",
+}
+
+var lastUsedPairs = make([]string, 0)
+
+func getThreeRandomPairs() []string {
+	rand.Seed(time.Now().UnixNano())
+
+	availablePairs := make([]string, len(pairs))
+	copy(availablePairs, pairs)
+
+	for _, usedPair := range lastUsedPairs {
+		for i, availablePair := range availablePairs {
+			if usedPair == availablePair {
+				availablePairs = append(availablePairs[:i], availablePairs[i+1:]...)
+				break
+			}
+		}
+	}
+
+	selectedPairs := make([]string, 0, 3)
+	for i := 0; i < 3; i++ {
+		index := rand.Intn(len(availablePairs))
+		selectedPairs = append(selectedPairs, availablePairs[index])
+		availablePairs = append(availablePairs[:index], availablePairs[index+1:]...)
+	}
+
+	lastUsedPairs = selectedPairs
+	return selectedPairs
+}
+
+func handlePairs(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	response := struct {
+		Pairs         []string `json:"pairs"`
+		RefreshPeriod int      `json:"refresh_period"`
+	}{
+		Pairs:         getThreeRandomPairs(),
+		RefreshPeriod: 1800,
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/pairs", handlePairs)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
